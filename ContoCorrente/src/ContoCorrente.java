54d8fdd3b3908c3b2e16208d15905dfa9f102cb5
Cirocancello75 <Cirocancello75.>
2021-12-28 13:16:44 +0100

    prova primo commit

---
+++ ContoCorrente/src/ContoCorrente.java
@@ -0,0 +1,39 @@
+
+public class ContoCorrente {
+
+	private double saldo;
+	private int numeroConto;
+	private static int ultimoNumeroConto=1000;
+		
+	public ContoCorrente(double saldo) {
+		this.saldo=saldo;	    			
+		numeroConto=ultimoNumeroConto;
+		ultimoNumeroConto++;
+		
+	}
+	
+	public void versa(double somma) {
+		saldo+=somma;
+		System.out.println("Sono stati versati "+ somma+" euro");
+	}
+	
+	public boolean preleva(double somma) {
+		if(saldo<somma) {
+			System.out.println("Prelievo fallito");
+			System.out.println("importo maggiore della disponibilità");
+			return false;
+		}else {
+			saldo-=somma;{
+			System.out.println("Sono stati prelevati "+somma+" euro");
+			return false;
+			}
+		}
+	}	
+		public double ottieniSaldo() {
+			return saldo;
+		}
+
+		public int ottieniNumeroConto() {
+			return numeroConto;
+		}
+}
---
+++ ContoCorrente/src/UsaConto.java
@@ -0,0 +1,29 @@
+
+public class UsaConto {
+
+	public static void main(String[] args) {
+		ContoCorrente conto1 = new ContoCorrente(100);
+		ContoCorrente conto2 = new ContoCorrente(200);
+		ContoCorrente conto3 = new ContoCorrente(1000);
+		
+		System.out.println("Saldo conto numero "+conto1.ottieniNumeroConto()+" euro "+conto1.ottieniSaldo());
+		System.out.println("Saldo conto numero "+conto2.ottieniNumeroConto()+" euro "+conto2.ottieniSaldo());
+		System.out.println("Saldo conto numero "+conto3.ottieniNumeroConto()+" euro "+conto3.ottieniSaldo());
+		
+		System.out.println("Prelevo dal conto numero "+conto1.ottieniNumeroConto()+" 50 €");
+		conto1.preleva(50);
+		
+		System.out.println("Prelevo dal conto numeri "+conto2.ottieniNumeroConto()+" 100 €");
+		conto2.preleva(100);
+		
+		System.out.println("Saldo conto numero "+conto1.ottieniNumeroConto()+" euro "+conto1.ottieniSaldo());
+		System.out.println("Saldo conto numero "+conto2.ottieniNumeroConto() +"euro "+conto2.ottieniSaldo());
+		
+		System.out.println("Prelevo dal conto numero "+conto1.ottieniNumeroConto()+" 100 €");
+		conto1.preleva(100);
+		
+		
+
+	}
+
+}
